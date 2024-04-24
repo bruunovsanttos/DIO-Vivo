26fec1141e7646908addd39eccd3cf719af2b5c3
bruunosanttos <bruno-vieira-santos@hotmail.com>
2024-04-23 22:16:28 -0300

    Operadores aritimeticos como eles funcionam e exmplos

---
+++ Operadores de atribuição.py
@@ -0,0 +1,15 @@
+#operadores de atribuição
+#adição
+print(1+1)
+#subtração
+print(2-1)
+#multiplicação
+print(1*5)
+#divisão
+print(10/2)
+#divisão inteiera
+print(10//2)
+#modulo (mostra a sobra da divisão que não sobra zero)
+print(10%3)
+#exponenciação
+print(2**3)
