c9d1c60c8a56ab09469615e7d3ff97ea2bf1a99d
bruunosanttos <bruno-vieira-santos@hotmail.com>
2024-05-28 21:59:11 -0300

    https://github.com/bruunovsanttos/DIO-Vivo.git

--- interfaces_classes_abstratas.py
+++ interfaces_classes_abstratas.py
@@ -2,3 +2,42 @@
 #é como um produto deve ser feito e não atribui como ele deve fazer
 #para garantir uma padronização de um produto ou processo
 #em python utilizamos classes abstratas para criar contratos, e classes abstratas não podem ser instanciadas
+
+#Abstract Basic Class, decora metodos da class base e registra classes concretas com inplementações da base abstrata utilizando o @abstractmethod
+
+from abc import ABC, abstractmethod
+class ControleRemoto(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+
+
+class ControleTv(ControleRemoto):
+    def ligar(self):
+        print("ligando TV")
+
+    def desligar(self):
+        print("Desligando a TV...")
+        print("Desligada!")
+
+
+class ControleArCondicionado(ControleRemoto):
+    def ligar(self):
+        print("ligando Ar Condicionado...")
+        print("Desligado")
+
+    def desligar(self):
+        print("Desligando o Ar Condicionado...")
+        print("Desligado!")
+
+controle = ControleTv()
+controle.ligar()
+controle.desligar()
+
+controle = ControleArCondicionado()
+controle.ligar()
+controle.desligar()
